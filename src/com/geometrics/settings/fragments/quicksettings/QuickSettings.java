674e49a6b61135df475197e86742eed5aa98cada
Ido Ben-Hur <idoybh2@gmail.com>
2024-05-02 08:52:58 +0200

    Geometrics: Allow turning on Bluetooth when pressing the tile [2/2]

--- src/com/geometrics/settings/fragments/quicksettings/QuickSettings.java
+++ src/com/geometrics/settings/fragments/quicksettings/QuickSettings.java
@@ -1,39 +1,18 @@
 package com.geometrics.settings.fragments.quicksettings;
 
 import android.os.Bundle;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
-import android.os.UserHandle;
-import android.content.ContentResolver;
-import android.content.res.Resources;
-import androidx.preference.ListPreference;
 import androidx.preference.Preference;
-import androidx.preference.PreferenceGroup;
+import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.PreferenceScreen;
-import androidx.preference.PreferenceCategory;
-import androidx.preference.Preference.OnPreferenceChangeListener;
-import androidx.preference.PreferenceFragment;
-import androidx.preference.SwitchPreference;
-import android.provider.Settings;
+import androidx.preference.Preference;
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.SettingsPreferenceFragment;
+
 import com.android.settings.R;
 
-import java.util.Locale;
-import android.text.TextUtils;
-import android.view.View;
-
-import com.android.settings.SettingsPreferenceFragment;
-import com.android.internal.logging.nano.MetricsProto;
-import com.android.settings.Utils;
-import android.util.Log;
-
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.HashMap;
-import java.util.Collections;
-
 public class QuickSettings extends SettingsPreferenceFragment implements Preference.OnPreferenceChangeListener {
+
+    private static final String KEY_QS_SHOW_AUTO_BRIGHTNESS = "qs_show_auto_brightness";
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
@@ -51,5 +30,15 @@ public class QuickSettings extends SettingsPreferenceFragment implements Prefere
 
         getActivity().setTitle(R.string.geometrics_qs_dashboard_title);
 
+        PreferenceScreen preferenceScreen = getPreferenceScreen();
+        Preference qsShowAutoBrightnessPreference = preferenceScreen.findPreference(KEY_QS_SHOW_AUTO_BRIGHTNESS);
+
+        if (qsShowAutoBrightnessPreference != null) {
+            boolean automaticBrightnessAvailable = getContext().getResources().getBoolean(
+                    com.android.internal.R.bool.config_automatic_brightness_available);
+            if (!automaticBrightnessAvailable) {
+                preferenceScreen.removePreference(qsShowAutoBrightnessPreference);
+            }
+        }
     }
 }
